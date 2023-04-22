d3f5e21b8f362fa4b400648ae428e6b6f9683f00
Sakshi <sakshi0317sv@gmail.com>
2023-04-22 16:52:34 +0530

    Second Commit

--- SQL_DVA.sql
+++ SQL_DVA.sql
@@ -5,3 +5,12 @@ use PRP_DVA
 select * from Transactions
 select * from Product
 select * from Customer
+
+--Q1. How many distinct customers are there who has returned the product at least once?
+
+select * from Transactions
+where sale_amount < 0
+
+--Q2. Find the top customer who redeemed the most reward points. What is their rank in terms of sales amount?
+
+
