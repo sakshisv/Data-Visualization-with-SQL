f2e79a8ad785b671fca2ba6508f1f3eef50eee5f
Sakshi <sakshi0317sv@gmail.com>
2023-04-22 16:39:28 +0530

    Second Commit

--- SQL_DVA.sql
+++ SQL_DVA.sql
@@ -5,37 +5,3 @@ use PRP_DVA
 select * from Transactions
 select * from Product
 select * from Customer
-
---Q1. How many distinct customers are there who has returned the product at least once?
-
-select * from Transactions
-where sale_amount < 0
-
---Q2. Find the top customer who redeemed the most reward points. What is their rank in terms of sales amount?
-
-select TOP 1 Customer_ID, Customer_value, Points_redeemed,
-DENSE_RANK() over (order by Customer_value) Cus_Rank
-from Customer  
-order by Points_redeemed desc
-
-With Q2 as(
-select b.Customer_ID, sum(a.sale_amount) as Sales, b.Points_redeemed
-from Transactions a
-left join Customer b
-on a.USER_ID = b.Customer_ID
-group by b.Customer_ID, b.Points_redeemed)
-
-select top 1 *, DENSE_RANK() over (order by Sales) Cus_Rank from Q2
-order by Points_redeemed desc
-
---Q3. What is the average transaction value by top 10 customers in terms of sales?
-
-With Q3 as(
-select TOP 10 b.Customer_ID, sum(a.sale_amount) as Sales
-from Transactions a
-left join Customer b
-on a.USER_ID = b.Customer_ID
-group by b.Customer_ID
-order by sum(sale_amount) desc) 
-
-select round(AVG(Sales),2) Average_Txn_Value from Q3
