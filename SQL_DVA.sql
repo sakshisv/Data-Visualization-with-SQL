411fb14efe362cd2fad344fd87ad5db5fb94bbc9
Sakshi <sakshi0317sv@gmail.com>
2023-04-23 23:23:46 +0530

    Third Commit

--- SQL_DVA.sql
+++ SQL_DVA.sql
@@ -13,4 +13,11 @@ where sale_amount < 0
 
 --Q2. Find the top customer who redeemed the most reward points. What is their rank in terms of sales amount?
 
+select TOP 1 Customer_ID, Customer_value, Points_redeemed,
+DENSE_RANK() over (order by Customer_value) Cus_Rank
+from Customer  
+order by Points_redeemed desc
+
+
+
 
